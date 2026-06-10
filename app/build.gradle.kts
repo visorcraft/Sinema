f0eb8c7dc5e739dca030ffcfab020f2672af6391
VisorCraft LLC <thomas@visorcraft.com>
2026-06-10 13:25:44 -0500

    Bump version to 1.12.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.sinema"
         minSdk = 24
         targetSdk = 36
-        versionCode = 15
-        versionName = "1.11.0"
+        versionCode = 16
+        versionName = "1.12.0"
     }
 
     buildTypes {
