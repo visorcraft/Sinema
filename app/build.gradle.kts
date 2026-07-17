e6337dd7d4522f7b9deb57eb9817a3910f3de40e
visorcraft <thomas@visorcraft.com>
2026-07-17 18:47:58 -0500

    Prepare release 1.16.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.sinema"
         minSdk = 24
         targetSdk = 36
-        versionCode = 20
-        versionName = "1.15.0"
+        versionCode = 21
+        versionName = "1.16.0"
     }
 
     buildTypes {
